a058abee0743c6c83ec9483b13ac2a01b266fb08
setmiller <setmiller@gmail.com>
2019-04-15 22:21:20 +0300

    add readme and comments for simple calc

--- Modules/01_simple calc/script.js
+++ Modules/01_simple calc/script.js
@@ -4,27 +4,35 @@
       value()
     }
 }(
+  // Функция для суммирования введенных значений  
   function calcInputValue(){
+    // Получаем доступ к NodeList элементов
     const btn = document.querySelectorAll('[name="calc"]')
     const span = document.querySelector('.res')
     const inputs = document.querySelectorAll('[name="num1"], [name="num2"]')
     const calc = document.querySelectorAll('.forCalc')
+    // При нажатии на input снимаем блокирование кнопки "="
     for(let input of inputs){
       input.onkeydown = function(){
         btn[0].disabled = false
       }
     }
+    // Отслеживаем какое арифметическое действие выбрали
     for (let item of calc){
       item.onclick = function(){
         return calcName = this.name
       }
     }
+    // Выполняем арифметическое действие с числами из input
     for (let btnElement of btn){
       btnElement.onclick = function(){
+        // Выполняем арифметическое действие при условии что все сделано верно
         try {calcValue(btnElement, calcName)}
+        // Выводим информацию в случае, если арифметическое действие не выбрано
         catch (err){console.log('Выберете действие !!!')}
       }
     }
+    // Функция отслеживаем какое действие выбрали, высчитываем результат и блокирует кнопку "="
     function calcValue(btnElement, calcName){
       const calc = {
         add: (x, y) => { return x + y },
@@ -32,9 +40,13 @@
         multiply: (x, y) => { return x * y },
         divide: (x, y) => { return x / y },
       }
-      const sum = [...inputs].map(input => parseInt(input.value))
-      const res = calc[calcName](sum[0], sum[1])
+      // Через замыкание получаем достук к input.value
+      const arr = [...inputs].map(input => parseInt(input.value))
+      // Вычисляем арифметическое действие
+      const res = calc[calcName](arr[0], arr[1])
+      // Проверяем что в оба input ввели числа (без доп. проверок) и заносим результат в span
       !res ? span.innerHTML = 'введите все числа' : span.innerHTML = res 
+      // Если результат внесен, то блокируем кнопку
       span.innerHTML == res ? btnElement.disabled = true : ''
     }
   }
--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-# some_small_modules
+# Модули
 
-Here I will place my small modules for further use
+## `Здесь я буду размещать небольшие наработки для дальнейшего использования при необходимости`
 
+### Перечень содержимого репозитория:
+
+* [01_simple calc](https://github.com/SetMiller/some_small_modules/tree/master/Modules/01_simple%20calc)
+* [02_item data price sum calc](https://github.com/SetMiller/some_small_modules/tree/master/Modules/02_item%20data%20price%20sum%20calc)
+* [03_confirmLinks and imgBlock](https://github.com/SetMiller/some_small_modules/tree/master/Modules/03_confirmLinks%20and%20imgBlock)
+* [04_simpleSlider_v0.1](https://github.com/SetMiller/some_small_modules/tree/master/Modules/04_simpleSlider_v0.1)
+* [05_simpleSliders_v0.4](https://github.com/SetMiller/some_small_modules/tree/master/Modules/05_simpleSliders_v0.4)
 
