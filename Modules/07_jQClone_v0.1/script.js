1e3442b52145775afb6e2afc36a1b70e5643066b
SetMiller <setmiller@gmail.com>
2019-04-23 02:03:54 +0300

    rename у ename ☁️

---
+++ Modules/07_jQClone_v0.1/script.js
@@ -0,0 +1,99 @@
+// Импортируем основную функцию
+  import JQ from './lib/jQClone.js'
+  // import {jQ, jQClone} from './lib/jQClone.js'
+  // import jQClone from './lib/jQClone.js'
+
+! function (...args) {
+  for (let arg of args){
+    arg()
+  }
+}(
+  function inputHighlighting(){
+    // получаем элементы
+    const inputs = document.querySelectorAll('.check')
+    const form = document.querySelector('form')
+    // при отправке формы проверяем условие выполнения функции
+    form.onsubmit = function(e){
+      eventWatching(e)
+    }
+    function eventWatching(e){
+      let error = false;
+      for (let input of inputs) {
+        // если инпут пустой, то добавляем класс и чекаем ошибку
+        if (input.value === '') {
+          input.classList.add('err'); 
+          error = true
+        }
+      }
+      // если есть ошибка, то прерываем выполнение эвента
+      if(error){
+          // прерываем выполнение отправки формы если есть ошибка
+          e.preventDefault();
+      }
+    }
+      // управление цветовыми индикаторами input
+      // for(let input of inputs){
+      //   input.onclick = function(){
+      //     this.classList.remove('err')
+      //   }
+      //   input.onfocus = function(){
+      //     this.classList.add('focus')
+      //   }
+      //   input.onblur = function(){
+      //     this.classList.remove('focus')
+      //   }
+      // }
+  },
+  function jQCloneOne(){
+    // 1️⃣
+      // for(let input of inputs){
+      //   input.addEventListener('blur', function(){
+      //     this.classList.remove('err')
+      //   })
+      // }
+
+    // 2️⃣
+      // let jqInputs = new jQclone(inputs)
+      // jqInputs.click(function(){
+      //       this.classList.remove('err')
+      //   }
+      // )
+  },
+  function jQCloneTwo(){
+    // 3️⃣
+      // let jqInputs = new jQClone(inputs)
+      // jqInputs.on('click', removeErr)
+      // jqInputs.on('focus', addFocus)
+      // jqInputs.on('blur', removeFocus)
+
+    // 4️⃣  Выбираем необходимые элементы и вешаем на них обработчики
+      let jqInputs = JQ('.check')
+      jqInputs
+              .on('click', removeErr)
+              .on('focus', addFocus)
+              .on('blur', removeFocus)
+
+      jqInputs
+              .addClass('some')
+              .removeClass('some')
+      
+    // Функции обработчики событий у инпутов
+      function removeErr(){
+        this.classList.remove('err')
+      }
+      function addFocus(){
+        this.classList.add('focus')
+      }
+      function removeFocus(){
+        this.classList.remove('focus')
+      }
+  },
+  function jQCloneThree(){ 
+    // 5️⃣ Получаем NodeList elements, добавляем в них текст и вешаем обработчик 
+      JQ('.items .item')
+                        .html('some text')
+                        .on('click', function(){
+                          this.style.color = 'red'
+                        })
+  }
+)
